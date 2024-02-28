e192e25b848d3eda62fb4b0edd37c93e7191b4ba
Samuel Swafford <samuelswafford00@gmail.com>
2024-02-28 17:18:34 -0600

    adding docstring to home() function

--- myapp/app.py
+++ myapp/app.py
@@ -5,6 +5,16 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    """
+    Renders the home page of the web application.
+
+    If the request method is POST, it retrieves the selected year from the form data and constructs
+    the URL for the corresponding plot. Then, it renders the index.html template with the selected year
+    and plot URL.
+
+    Returns:
+        The rendered index.html template with the selected year and plot URL.
+    """
     year = None
     plot_url = None
     if request.method == 'POST':
