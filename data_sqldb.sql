3a3547427ddf3f65301a5f6cf111da830c36f6e6
AddaGould <addagould@gmail.com>
2024-02-29 18:43:55 -0600

    add sql schema

---
+++ data_sqldb.sql
@@ -0,0 +1,18 @@
+SELECT * FROM "Data";
+
+DROP TABLE "Data";
+
+CREATE TABLE "Data" (
+    "Date" VARCHAR,
+    "CityName" VARCHAR,
+    "StateName" VARCHAR,
+    "MedianSalePrice" FLOAT,
+    "Year" FLOAT,
+    "MedianListPrice" FLOAT,
+    "SalesTotal" FLOAT,
+    "Total Population" FLOAT,
+    "Median Income" FLOAT,
+    "MORTGAGE30US" FLOAT,
+    "Unemployment Rate" FLOAT,
+    "HAI" FLOAT
+);
