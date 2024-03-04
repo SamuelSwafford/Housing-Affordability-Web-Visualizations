5ba4c2baeaecdb145e885c0caf06e4f7a683a4ec
Samuel Swafford <samuelswafford00@gmail.com>
2024-03-04 14:32:58 -0600

    changes

--- myapp/bar_and_line_plot.py
+++ myapp/bar_and_line_plot.py
@@ -4,11 +4,17 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def plot_bar_and_line( city = 'United States'):  # Renamed function
+def plot_bar_and_line( city = 'United States', col='Chicago'):  # Renamed function
     df = pd.read_csv('resources/data_interpolated.csv')
+    allowed_columns = df.select_dtypes(include='number').columns.tolist()
+    allowed_columns.remove('Year')
+    allowed_columns.remove('SalesTotal')
+    allowed_columns = set(allowed_columns)
+    if col not in allowed_columns:
+        raise ValueError(f'Invalid value for col. Allowed values are {allowed_columns}')
     df = df[df['CityName'] == city]
-    df = df[['Date', 'SalesTotal', 'MORTGAGE30US']]
-    df['MORTGAGE30US'] = df['MORTGAGE30US'].bfill()
+    df = df[['Date', 'SalesTotal', col]]
+    df[col] = df[col].bfill()
     df.dropna(subset=['SalesTotal'], inplace=True)
 
     # Convert 'Date' to datetime and set it as index
@@ -16,7 +22,7 @@ def plot_bar_and_line( city = 'United States'):  # Renamed function
     df.set_index('Date', inplace=True)
 
     # Calculate yearly sum of 'SalesTotal' and yearly average of 'MORTGAGE30US'
-    df = df.resample('Y').agg({'SalesTotal': 'sum', 'MORTGAGE30US': 'mean'})
+    df = df.resample('Y').agg({'SalesTotal': 'sum', col: 'mean'})
 
     # Plot the data
     fig, ax1 = plt.subplots()
@@ -26,16 +32,16 @@ def plot_bar_and_line( city = 'United States'):  # Renamed function
     bar = ax2.bar(df.index.year, df['SalesTotal'], color='blue', zorder=0, alpha=.5)
 
     # Line plot
-    line, = ax1.plot(df.index.year, df['MORTGAGE30US'], color='red', zorder=1)
+    line, = ax1.plot(df.index.year, df[col], color='red', zorder=1)
 
     # Set labels and title
     ax1.set_xlabel('Time')
-    ax1.set_ylabel('MORTGAGE30US')
+    ax1.set_ylabel(col)
     ax2.set_ylabel('Sales Total')
-    plt.title('Mortgage Rate and Sales Total Over Time for ' + city)
+    plt.title(col+ ' and Sales Total Over Time for ' + city)
 
     # Add legends
-    plt.legend([line, bar], ['MORTGAGE30US', 'Sales Total'])
+    plt.legend([line, bar], [col, 'Sales Total'])
 
     # Move the line plot to the front
     ax1.set_zorder(ax2.get_zorder() + 1)
@@ -43,9 +49,9 @@ def plot_bar_and_line( city = 'United States'):  # Renamed function
     plt.savefig('static/bar_and_line_plot.svg')
 
     plt.close()
+
 if __name__ == '__main__':
     # Example usage
-    city = 'United States'
-    # column1 = 'SalesTotal'
-    # column2 = 'MORTGAGE30US'
-    plot_bar_and_line(city)
+    city = 'Chicago'
+    col = 'MORTGAGE30US'
+    plot_bar_and_line(city, col)
